e36ab538d8ae55d4f84f0d304d0c72e6b70f3912
samuel <slavka.samuel@gmail.com>
2021-03-06 11:24:28 +0100

    removed search suggestions
    sped up api

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors')
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-
-const path = __dirname + '/views/';
 const port = 8080;
 
+app.use(cors())
+
 app.get('/', function(req, res){
-	var key = req.query['api-key'];
-	res.send('hello');
+	res.send("no input");
 });
 
-
 app.get('/:val', async function(req, res, next){
-	var cont = await searchitem(req.params.val);
+	var cont = await getContents(req.params.val);
 	result = [];
 	cont.forEach(element => {
+		// remove endline chars
 		result.push(element.replace(/[\n\r]/g,' '))
 	});
 	
@@ -25,70 +24,36 @@ app.get('/:val', async function(req, res, next){
 });
   
 
-//execute search in wikipedia
-async function searchitem(str)
-{
-	try {
-		const { data } = await axios.get(
-			'https://en.wikipedia.org/w/index.php?search='+str+'&title=Special:Search&profile=advanced&fulltext=1&advancedSearch-current={}&ns0=1'
-		);
-		var $ = cheerio.load(data);
-		var result = [];
-		$('div.mw-parser-output:empty').remove();
-		$('p.mw-empty-elt').remove();
-		
-		if ($('p.mw-search-exists').text() != ''){
-			result = await getContents(str);
-		}
-		else if ($('p.mw-search-nonefound') != ''){
-			result.push($('p.mw-search-nonefound').text());    
-		}
-		else {
-			if ($('div.searchdidyoumean') != '')
-				result.push($('div.searchdidyoumean').text());   
-			else result.push($('p.mw-search-createlink').text());    
-
-			var newterm = $('ul.mw-search-results > li.mw-search-result > div.mw-search-result-heading > a:first').text();
-			result.push( await getContents(newterm));   
-			result = [].concat(...result);
-		}	
-
-		return result;
-	} catch (error) {
-		throw error;
-	}
-};
-
 //return contents of article, either first paragraph or itemize
 async function getContents(str) 
 {
-	try {
+	try {		
 		const { data } = await axios.get(
 			'https://en.wikipedia.org/wiki/'+str
 		);
 		const $ = cheerio.load(data);
-    
+		
 		$('div.mw-parser-output:empty').remove();
 		$('p.mw-empty-elt').remove();
-		var content = []
-		content.push($('div.mw-parser-output > p:first').text());
 		
-
-		if (content[0].includes('refer to:')){
+		var content = []
+		content.push($('div.mw-parser-output > p:first').text());	
+		//list of items
+		if (content[0].includes('may refer to:')){
 			$('div.mw-parser-output > ul').each((_idx, el) => {
 				const item = $(el).text();
-				content.push(item)
+				content.push(item);
 			});
 		}
 		
 		return content;
-	} catch (error) {
-		throw error;
+	} catch (error) {	
+		// no article for topic
+		return ['Wikipedia does not have an article with this exact name.'];
 	}
 };
 
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port '+ port);
 })
-
