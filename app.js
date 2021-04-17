ef4d425d4cf458c8283dfab32528a1a384757031
salgovic.marek@gmail.com <salgovic.marek@gmail.com>
2021-04-17 16:27:53 +0200

    feat(image): add image endpoint

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const app = express();
 const cors = require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const url = require('url');
 const languages = require('./languages');
 
 const port = 8080;
 
 
 app.use(cors());
-
-app.get('/', function (req, res) {
-    res.send("no input");
-});
 
 function verifyLanguage(lang) {
     if (!lang) {
@@ -24,11 +21,16 @@ function verifyLanguage(lang) {
     return {valid: true, lang: lang}
 }
 
-app.get('/:val', async function (req, res, next) {
+app.get('/', function (req, res) {
+    res.status(404).type('application/json').send({error: "method not found"});
+});
+
+
+app.get('/articles/:val', async function (req, res, next) {
     let verifiedLanguage = verifyLanguage(req.query.lang);
 
-    if(!verifiedLanguage.valid) {
-        res.status(400).type('application/json').send({error: 'Wikipedia does not support ' + verifiedLanguage.lang + ' language'})
+    if (!verifiedLanguage.valid) {
+        res.status(400).type('application/json').send({error: 'Wikipedia does not support ' + verifiedLanguage.lang + ' language'});
         return
     }
     var cont = await getContents(req.params.val, verifiedLanguage.lang);
@@ -42,7 +44,36 @@ app.get('/:val', async function (req, res, next) {
     } else {
         res.status(404).type('application/json').send({error: 'Wikipedia does not have an article with this exact name.'})
     }
+});
 
+
+app.get('/articles/:val/images', async function (req, res, next) {
+    let article = req.params.val;
+    let verifiedLanguage = verifyLanguage(req.query.lang);
+
+    if (!verifiedLanguage.valid) {
+        res.status(400).type('application/json').send({error: 'Wikipedia does not support ' + verifiedLanguage.lang + ' language'});
+        return
+    }
+    try {
+        const {data} = await axios.get(
+            'https://' + verifiedLanguage.lang + '.wikipedia.org/wiki/' + article
+        );
+        const $ = cheerio.load(data);
+
+        let image = $('.infobox').find('img').attr('src');
+        if (!!image) {
+            image = 'https:' + image
+        } else {
+            image = null;
+        }
+
+
+        res.status(200).type('application/json').send({image: image})
+    } catch (error) {
+        console.log(error)
+        res.status(404).type('application/json').send({error: 'Wikipedia does not have an article with this exact name.'})
+    }
 });
 
 
